c42d86dcb03b46fd1a5e0de77f9b7c8e1ad44967
weldson <weldson.amaral@gmail.com>
2020-03-25 07:59:43 -0300

    [Changelog]
    Configure react-native project using the github api

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';;
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+      </Form>
+      <SubmitButton>
+        <Icon name="add" size={20} color="#fff"/>
+      </SubmitButton>
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
+ 
